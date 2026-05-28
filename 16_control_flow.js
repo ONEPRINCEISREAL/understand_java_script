d2e1f1fa9c316090aadaebaccfd756f098c44e01
Prince singh Chauhan <Princesinghchauhan3007@gmail.com>
2026-05-29 01:24:24 +0530

    java script basics

---
+++ 16_control_flow.js
@@ -0,0 +1,73 @@
+// if
+
+const isUserLoggedIn = true;
+const temperature = 43;
+
+if (isUserLoggedIn) {
+// if (2==2) {
+//     console.log("This is true");
+// }
+// console.log("User is logged in");
+}
+
+if (temperature <= 24
+) {
+    // console.log("It's too hot outside");
+}else{
+    // console.log("It's not too hot outside");
+}
+// console.log("End of the code");
+
+// if (2==2) {
+//     console.log("This is true");
+// }
+
+if (2==="2") {
+    // console.log("This is true");
+}
+// 3!=2 // true
+
+// <,>,<=,>=,!=,!==
+
+// == // loose equality operator
+
+// === // strict equality operator
+
+
+
+
+// const score =200
+
+// if (score>100){
+//     const power = "Super Strength";
+//     console.log(`User has ${power}`);   
+// }
+// console.log(`User has ${power}`);   
+
+
+const balance = 1000;
+// if (balance > 500) console.log("User has a high balance");console.log("End of the code"); //avoid this
+//     {
+//     const discount = 0.1;
+//     console.log(`User gets a discount of ${discount * 100}%`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
